d9205e7aa62b44d45059c6363a7f9054256ea68a
mrt39 <mertoktar39@gmail.com>
2022-12-01 21:22:59 +0300

    Add score keeping and winner announcement

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ console.log("Hello World");
 function playRound(playerSelection, computerSelection) {
 
     let result = ""
+    let winner = ""
 
     if (playerSelection == computerSelection) {
         result = "DRAW"
@@ -46,43 +47,91 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "ROCK") {
         if (computerSelection == "SCISSORS") {
-            result = "You Win! Rock beats Scissors"    
+            result = "You Win! Rock beats Scissors" 
+            winner = "PLAYER"   
         } 
         else if (computerSelection == "PAPER") {
             result = "You Lose! Paper beats Rock"
+            winner = "COMPUTER"
         } 
     } 
 
     if (playerSelection == "PAPER") {
         if (computerSelection == "ROCK") {
             result = "You Win! Paper beats Rock"
+            winner = "PLAYER"
         } 
         else if (computerSelection == "SCISSORS") {
             result = "You Lose! Scissors beats Paper"
+            winner = "COMPUTER"
         } 
     } 
 
     if (playerSelection == "SCISSORS") {
         if (computerSelection == "ROCK") {
             result = "You Lose! Rock beats Scissors"
+            winner = "COMPUTER"
         } 
         else if (computerSelection == "PAPER") {
             result = "You Win! Scissors beats Paper"
+            winner = "PLAYER"
         } 
     } 
 
     console.log(result)
-    return result
+    return winner
     
   }
 
 function game(times) {
+
+    var playerWinCount = 0
+    var computerWinCount = 0
+
     for (let i = 0; i < times; i++) {
+
         //we get the playerchoice and computerchoice for every round
         var playerSelection = getPlayerChoice();
         var computerSelection = getComputerChoice(); 
-        playRound(playerSelection, computerSelection);
+        winnerOfRound = playRound(playerSelection, computerSelection);
+
+        if (winnerOfRound == "PLAYER"){
+            playerWinCount ++ 
+        } 
+
+        else if (winnerOfRound == "COMPUTER"){
+            computerWinCount ++ 
+        } 
      }
+
+    if (playerWinCount == computerWinCount){
+        console.log(
+        `SCORE:
+        PLAYER: ${playerWinCount} 
+        COMPUTER: ${computerWinCount}
+        
+        IT'S A DRAW! `)
+    } 
+
+    else if (playerWinCount > computerWinCount){
+        console.log(
+        `SCORE:
+        PLAYER: ${playerWinCount} 
+        COMPUTER: ${computerWinCount}
+        
+        YOU WIN! `)
+    } 
+
+    else if (playerWinCount < computerWinCount){
+        console.log(
+        `SCORE:
+        PLAYER: ${playerWinCount} 
+        COMPUTER: ${computerWinCount}
+        
+        COMPUTER WINS! `)
+    } 
+
+
  }
 
  
